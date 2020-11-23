aa95cfad91e9af867b21879cc082aa43cd6a7421
clearlove7 <zewen.liu@postgrad.manchester.ac.uk>
2020-11-23 16:56:09 +0000

    error debug

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?php
 print("Hello World!");
 
->
+/*
+<?php
+$database_host = "dbhost.cs.man.ac.uk";
+$database_user = "mbaxszlh";
+$database_pass = "<the password you set>";
+$database_name = "mbaxszlh";
+?>
+*/
+
+?>
+
+
